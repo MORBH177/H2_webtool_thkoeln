62cc33540832c6b39119fba15216a9c7dbdec393
MORBH177 <73952525+MORBH177@users.noreply.github.com>
2023-02-08 13:47:08 +0100

    update

--- calc.js
+++ calc.js
@@ -2,27 +2,23 @@
 function updateText1(val) {
   document.getElementById("textPv").value = val;
   updateMax();
-  pPv = parseInt(val);
 }
 
 //Update Textfeld anhand von Sliderstellung
 function updateText2(val) {
   document.getElementById("textWindC").value = val;
   updateMax();
-  pWindC = parseInt(val);
 }
 
 //Update Textfeld anhand von Sliderstellung
 function updateText3(val) {
   document.getElementById("textWindO").value = val;
   updateMax();
-  pWindO = parseInt(val);
 }
 
 //Update Textfeld anhand von Sliderstellung
 function updateText4(val) {
   document.getElementById("textPem").value = val;
-  pPem = parseInt(val);
 }
 
 //update Slider PEM als maximal Summe aller Generatorleistungen
@@ -32,5 +28,56 @@ function updateMax() {
   document.getElementById("sl4").max = max * maxUse;
 }
 
+function updateSlider1(val){
+  if (val < 0) {
+    document.getElementById("sl1").value = 0;
+    document.getElementById("textPv").value = 0;
+  } else if (val > 100) {
+    document.getElementById("sl1").value = 100;
+    document.getElementById("textPv").value = 100;
+  } else {
+    document.getElementById("sl1").value = val;
+  }
+  updateMax();
+}
 
+function updateSlider2(val){
+  if (val < 0) {
+    document.getElementById("sl2").value = 0;
+    document.getElementById("textWindC").value = 0;
+  } else if (val > 100) {
+    document.getElementById("sl2").value = 100;
+    document.getElementById("textWindC").value = 100;
+  } else {
+    document.getElementById("sl2").value = val;
+  }
+  updateMax();
+}
 
+function updateSlider3(val){
+  if (val < 0) {
+    document.getElementById("sl3").value = 0;
+    document.getElementById("textWindO").value = 0;
+  } else if (val > 100) {
+    document.getElementById("sl3").value = 100;
+    document.getElementById("textWindO").value = 100;
+  } else {
+    document.getElementById("sl3").value = val;
+  }
+  updateMax();
+}
+
+function updateSlider4(val){
+  const maxUse = 0.8;
+  var max = parseInt(document.getElementById("sl1").value) + parseInt(document.getElementById("sl2").value) + parseInt(document.getElementById("sl3").value);
+  updateMax();
+  if (val < 0) {
+    document.getElementById("sl4").value = 0;
+    document.getElementById("textPem").value = 0;
+  } else if (val > max * maxUse) {
+    document.getElementById("sl4").value = parseInt(max * maxUse);
+    document.getElementById("textPem").value = parseInt(max * maxUse);
+  } else {
+    document.getElementById("sl4").value = val;
+  }
+}
